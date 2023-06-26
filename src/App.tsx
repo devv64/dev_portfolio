523c7de242f8c4838e7e20065e8fa6fe9751112c
Dev <devextraemail@gmail.com>
2023-06-26 01:54:23 -0400

    Working version without theme switching

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,6 @@ import PortfolioPage from './components/PortfolioPage';
 import About from './components/About';
 
 const App: React.FC = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const [theme, setTheme] = useState<string[]>(['#00A6FB', '#001427', '#0054A2', '#DBDFAC']);
-
-  const pickTheme = () => {
-    const portfolioPageElement = document.getElementById('portfolio-page');
-    const portfolioPageOffsetTop = portfolioPageElement?.offsetTop || 0;
-    if (scrollPosition + 16 <= portfolioPageOffsetTop) {
-      setTheme(['#00A6FB', '#001427', '#0054A2', '#DBDFAC']);
-    }
-    else if (scrollPosition < 2160) {
-      setTheme(['#FAD4D8', '#001427', '#DBDFAC', '#0054A2']);
-    }
-    else {
-      setTheme(['#001427', '#DBDFAC', '#FAD4D8', '#0054A2']);
-    }
-  };
-
-  window.addEventListener('scroll', () => {
-    setScrollPosition(window.scrollY);
-    pickTheme();
-  }
-  );
-
   const links = [
     { title: 'Portfolio', url: '/portfolio' },
     { title: 'About', url: '/about' },
@@ -38,12 +15,13 @@ const App: React.FC = () => {
 
   return (
     <div>
-      <Navbar title="dp" links={links} theme={theme} />
+      <Navbar title="dp" links={links} />
       <HomePage portfolioRef={ref}/>
       <div id='portfolio-page'ref={ref}>
         <PortfolioPage />
       </div>
       {/* <About /> */}
+      <div className='p-40'></div>
     </div>
   );
 };
