93cdb9d627d7791ba1afa4c8c81bd9509415fe43
Dev <devextraemail@gmail.com>
2023-06-28 21:43:22 -0400

    Changed portfolio card structure, now pops up with description and maybe video

--- src/components/PortfolioCard.tsx
+++ src/components/PortfolioCard.tsx
@@ -1,22 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoMdArrowDropright } from 'react-icons/io';
 import Masonry from 'react-masonry-css';
 
 interface PortfolioItem {
+  videoUrl: any;
   title: string;
   description: string;
   imageUrl: string;
   tag: string;
-  colSpan: number;
-  rowSpan: number;
   link: string;
+  shortdesc: string;
 }
 
 interface PortfolioCardProps {
   items: PortfolioItem[];
 }
 
+interface ExpandedCardProps {
+  item: PortfolioItem;
+  onClose: () => void;
+}
+
+
+const ExpandedCard: React.FC<ExpandedCardProps> = ({ item, onClose }) => {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-80">
+      <div className="max-w-lg w-full p-8 bg-white rounded-lg">
+        <h3 className="text-2xl font-semibold text-gray-800 mb-4">{item.title}</h3>
+        <div className="flex justify-between mb-4">
+          <img
+            src={item.imageUrl}
+            alt={item.title}
+            className="h-32 w-32 object-cover rounded-lg"
+          />
+          <div className="ml-4">
+            <p className="text-gray-600">
+              <span className="font-semibold">Tag: </span>
+              {item.tag}
+            </p>
+            <p className="text-gray-600">
+              <span className="font-semibold">Link: </span>
+              <a href={item.link} className="text-blue-500 hover:underline">
+                {item.link}
+              </a>
+            </p>
+          </div>
+        </div>
+        <p className="text-gray-600">{item.description}</p>
+        {item.videoUrl !== '' && (
+          <div className="mt-4">
+            <video controls className="w-full">
+              <source src={item.videoUrl} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+        )}
+        <div className="flex justify-end mt-6">
+          <button
+            className="text-gray-500 hover:text-gray-700"
+            onClick={onClose}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+
 const PortfolioCard: React.FC<PortfolioCardProps> = ({ items }) => {
+  const [selectedCardIndex, setSelectedCardIndex] = useState<number | null>(null);
+
   const breakpointColumnsObj = {
     default: 4,
     1200: 3,
@@ -26,9 +82,9 @@ const PortfolioCard: React.FC<PortfolioCardProps> = ({ items }) => {
 
   return (
     <section id="portfolio" className="portfolio-section">
-      <section className="bg-[#FAD4D8] py-12">
+      <section className="bg-[#001427] py-12">
         <div className="container mx-auto">
-          <div className="text-3xl text-[#001427] pt-16 font-bold pl-2">PORTFOLIO</div>
+          <div className="text-3xl text-[#DBDFAC] pt-16 font-bold pl-2">PORTFOLIO</div>
           <Masonry
             breakpointCols={breakpointColumnsObj}
             className="my-masonry-grid"
@@ -37,33 +93,47 @@ const PortfolioCard: React.FC<PortfolioCardProps> = ({ items }) => {
             {items.map((item, index) => (
               <div
                 key={index}
-                className={`col-span-${item.colSpan} row-span-${item.rowSpan} relative`}
+                onClick={() => setSelectedCardIndex(index)}
               >
                 <div className="relative mt-4">
-                  <img src={item.imageUrl} alt={item.title} className="h-full w-full object-cover rounded-lg" />
+                  <img
+                    src={item.imageUrl}
+                    alt={item.title}
+                    className="h-[22rem] w-full object-cover rounded-lg"
+                  />
                   <div className="absolute top-0 left-0 p-2">
-                    <h3 className="text-s ml-1">{item.title}</h3>
+                    <h3 className="text-sm ml-1">{item.title}</h3>
                   </div>
-                  <a href={item.link} className="absolute top-0 right-0 p-2">
-                    <p className="text-white bg-[#00A6FB] rounded-lg px-2 py-0.5 text-xs">{item.tag}</p>
+                  <a href={item.link} className="absolute top-1.5 right-1.5">
+                    <p className="text-white bg-[#00A6FB] rounded-lg px-2 py-0.5 text-xs">
+                      {item.tag}
+                    </p>
+                  </a>
+                  <a
+                    className="absolute inset-0 flex items-center justify-center bg-black opacity-0 hover:opacity-80 transition-opacity duration-300 rounded-lg"
+                  >
+                    <div className="text-center p-4">
+                      <h3 className="text-2xl font-semibold text-white mb-2">
+                        {item.title}
+                      </h3>
+                      <p className="text-gray-200">{item.shortdesc}</p>
+                      <div className="mt-4 inline-flex items-center text-white font-semibold hover:text-[#00A6FB] transition-colors duration-200">
+                        View More <IoMdArrowDropright className="ml-1" />
+                      </div>
+                    </div>
                   </a>
                 </div>
-                <a href={item.link} className="absolute inset-0 rounded-lg bg-black opacity-0 hover:opacity-80 transition-opacity duration-300 flex items-center justify-center">
-                  <div className="text-center p-4">
-                    <h3 className="text-2xl font-semibold text-white mb-2">{item.title}</h3>
-                    <p className="text-gray-200">{item.description}</p>
-                    <div
-                      className="mt-4 inline-flex items-center text-white font-semibold hover:text-[#00A6FB] transition-colors duration-200"
-                    >
-                      View More <IoMdArrowDropright className="ml-1" />
-                    </div>
-                  </div>
-                </a>
               </div>
             ))}
           </Masonry>
         </div>
       </section>
+      {selectedCardIndex !== null && (
+        <ExpandedCard
+          item={items[selectedCardIndex]}
+          onClose={() => setSelectedCardIndex(null)}
+        />
+      )}
     </section>
   );
 };
--- src/components/PortfolioPage.tsx
+++ src/components/PortfolioPage.tsx
@@ -1,62 +1,75 @@
 import React, { forwardRef } from 'react';
 import PortfolioCard from './PortfolioCard';
+import CMS from '../assets/CMS.png'
+import meek from '../assets/meek.mp4'
+import blog from '../assets/blog.jpg'
+import splitit from '../assets/splitit.png'
 
 // const PortfolioPage: React.FC = () => {
 const PortfolioPage = forwardRef<HTMLDivElement>((props, ref) => {
   const portfolioItems = [
     {
-      title: 'Project 1',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      imageUrl: 'http://devv64.pythonanywhere.com/static/images/sageGiraffeReading.png',
+      title: 'SG Headless CMS',
+      shortdesc: 'A headless CMS built with Flask and React',
+      description: 'As part of the Sage Giraffe CMS project, I played a key role in developing the database and API, along with contributing to the frontend. This highly versatile and developer-friendly headless CMS offers advanced functionality for creating, managing, updating, and deleting posts and users. With seamless integration capabilities and intuitive interfaces, it enables the creation of dynamic content across multiple platforms while ensuring efficient user management and granular control over data.',
+      imageUrl: CMS,
       tag: 'Flask',
-      colSpan: 3,
-      rowSpan: 3,
       link: 'http://devv64.pythonanywhere.com/',
+      videoUrl: '',
     },
     {
-      title: 'Project 2',
-      description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
-      imageUrl: 'https://media.graphassets.com/9kTzcfcVQqILlQwqOoPg',
+      title: 'Dev\'s Blog Site',
+      shortdesc: 'A blog site built with Next.js and Tailwind CSS',
+      description: 'Blogsite is a dynamic website built with React, Next.js, GraphQL, and Tailwind CSS. It offers a seamless browsing experience and utilizes GraphQL for efficient storage of blog posts and comments. Explore a collection of engaging articles and interact with the content effortlessly.',
+      imageUrl: blog,
       tag: 'React',
-      colSpan: 2,
-      rowSpan: 3,
       link: 'https://devsblogsite.vercel.app/',
+      videoUrl: '',
+    },
+    {
+      title: 'SplitIt',
+      shortdesc: 'A receipt splitting app built with React, Firebase, and Tailwind CSS',
+      description: 'SplitIt is a dynamic web application created during a hackathon in April 2023. Powered by React, Firebase, and Tailwind CSS, it simplifies the process of splitting receipts among friends or groups. With intuitive user interfaces and real-time data storage, SplitIt ensures a seamless and efficient experience. Utilizing Firebase, user data is securely stored, and personalized statistics are generated on the user dashboard, providing valuable insights.',
+      imageUrl: splitit,
+      tag: 'React',
+      link: 'https://splitit-djs.vercel.app/',
+      videoUrl: '',
     },
     {
       title: 'Project 1',
+      shortdesc: 'A blog site built with Next.js and Tailwind CSS',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       imageUrl: 'http://devv64.pythonanywhere.com/static/images/sageGiraffeReading.png',
       tag: 'Flask',
-      colSpan: 3,
-      rowSpan: 3,
       link: 'http://devv64.pythonanywhere.com/',
+      videoUrl: '',
     },
     {
       title: 'Project 1',
+      shortdesc: 'A blog site built with Next.js and Tailwind CSS',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       imageUrl: 'http://devv64.pythonanywhere.com/static/images/sageGiraffeReading.png',
       tag: 'Flask',
-      colSpan: 3,
-      rowSpan: 5,
       link: 'http://devv64.pythonanywhere.com/',
+      videoUrl: '',
     },
     {
       title: 'Project 1',
+      shortdesc: 'A blog site built with Next.js and Tailwind CSS',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       imageUrl: 'http://devv64.pythonanywhere.com/static/images/sageGiraffeReading.png',
       tag: 'Flask',
-      colSpan: 3,
-      rowSpan: 1,
       link: 'http://devv64.pythonanywhere.com/',
+      videoUrl: '',
     },
     {
       title: 'Project 1',
+      shortdesc: 'A blog site built with Next.js and Tailwind CSS',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       imageUrl: 'http://devv64.pythonanywhere.com/static/images/sageGiraffeReading.png',
       tag: 'Flask',
-      colSpan: 3,
-      rowSpan: 3,
       link: 'http://devv64.pythonanywhere.com/',
+      videoUrl: '',
     },
   ];
 
