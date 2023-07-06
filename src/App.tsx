cb2db029bfdc8e0fd1f979fcd67969baea240bbd
Dev <devextraemail@gmail.com>
2023-07-06 03:00:38 -0400

    Added recent tracks, linkedin, commits to about section, fixed mobile styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import PortfolioPage from './components/PortfolioPage';
 import About from './components/About';
+import { Element } from 'react-scroll';
 
 const App: React.FC = () => {
   const links = [
@@ -15,16 +16,18 @@ const App: React.FC = () => {
 
   return (
     <div>
-      <Navbar title="dp" links={links} />
+      <Navbar title="dp"/>
       <HomePage portfolioRef={ref}/>
-      <div id='portfolio-page'ref={ref}>
+      <Element name="portfolio" className="element">
         <PortfolioPage />
-      </div>
-      <About 
-        imageSrc="https://media.gettyimages.com/id/685132245/photo/mature-businessman-smiling-over-white-background.jpg?s=612x612&w=gi&k=20&c=ELBLmT-KQ6vhI5xiWnQcWWw0QJiELKt6STlga4fwRC8="
-        title="About Me"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis, elit consectetur gravida feugiat, dui erat fringilla nulla, sed tincidunt est ex ac ligula. Sed pretium orci sed egestas laoreet. Cras consectetur eros quis nulla sagittis, at porta ante porttitor. Maecenas commodo turpis vel vestibulum tincidunt. Aenean leo nibh, tristique interdum facilisis at, tristique nec nulla. Etiam hendrerit mi eu purus iaculis, vitae consequat nibh laoreet. Ut id nulla ipsum. Donec suscipit luctus augue, et suscipit elit scelerisque a. Nam non turpis a lacus gravida euismod. Ut eget augue nec est pretium venenatis. Vivamus enim nisl, hendrerit eget rhoncus ac, posuere vel dolor. Suspendisse quis accumsan massa, a pulvinar tortor. Cras et pulvinar turpis."
+      </Element>
+      <Element name="about" className="element">
+        <About 
+          imageSrc="https://media.gettyimages.com/id/685132245/photo/mature-businessman-smiling-over-white-background.jpg?s=612x612&w=gi&k=20&c=ELBLmT-KQ6vhI5xiWnQcWWw0QJiELKt6STlga4fwRC8="
+          title="About Me"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis, elit consectetur gravida feugiat, dui erat fringilla nulla, sed tincidunt est ex ac ligula. Sed pretium orci sed egestas laoreet. Cras consectetur eros quis nulla sagittis, at porta ante porttitor. Maecenas commodo turpis vel vestibulum tincidunt. Aenean leo nibh, tristique interdum facilisis at, tristique nec nulla. Etiam hendrerit mi eu purus iaculis, vitae consequat nibh laoreet. Ut id nulla ipsum. Donec suscipit luctus augue, et suscipit elit scelerisque a. Nam non turpis a lacus gravida euismod. Ut eget augue nec est pretium venenatis. Vivamus enim nisl, hendrerit eget rhoncus ac, posuere vel dolor. Suspendisse quis accumsan massa, a pulvinar tortor. Cras et pulvinar turpis."
         />
+      </Element>
     </div>
   );
 };
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { motion } from 'framer-motion';
+import axios from 'axios';
+import LASTFM_API_KEY from './config';
+import './About.css';
+
+declare global {
+  interface Window {
+    IN: any;
+  }
+}
 
 interface AboutProps {
   imageSrc: string;
   title: string;
   description: string;
+}
+
+interface PlayHistoryObject {
+  artist: {
+    mbid: string;
+    '#text': string;
+  };
+  name: string;
+  image: string;
 }
 
 const About: React.FC<AboutProps> = ({ imageSrc, title, description }) => {
@@ -14,13 +32,67 @@ const About: React.FC<AboutProps> = ({ imageSrc, title, description }) => {
     threshold: 0.4,
   });
 
+  const [recentlyPlayed, setRecentlyPlayed] = useState<PlayHistoryObject[]>([]);
+
+  const fetchRecentlyPlayed = async () => {
+    try {
+      const response = await axios.get(
+        `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=devv64&api_key=${LASTFM_API_KEY}&format=json&limit=5`
+      );
+      const tracks: PlayHistoryObject[] = response.data.recenttracks.track;
+      const updatedTracks: PlayHistoryObject[] = [];
+
+      for (const track of tracks) {
+        const trackInfoResponse = await axios.get(
+          `http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${LASTFM_API_KEY}&artist=${encodeURIComponent(
+            track.artist['#text']
+          )}&track=${encodeURIComponent(track.name)}&format=json`
+        );
+        const trackInfo = trackInfoResponse.data.track;
+
+        const updatedTrack: PlayHistoryObject = {
+          ...track,
+          image: trackInfo.album?.image?.[3]['#text'] || '',
+        };
+
+        updatedTracks.push(updatedTrack);
+      }
+
+      setRecentlyPlayed(updatedTracks);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecentlyPlayed();
+  }, []);
+
   const fadeAnimation = {
     hidden: { opacity: 0 },
     visible: { opacity: 1 },
   };
 
+  useEffect(() => {
+    const loadLinkedInSDK = () => {
+      const script = document.createElement('script');
+      script.src = 'https://platform.linkedin.com/badges/js/profile.js';
+      script.async = true;
+      script.defer = true;
+      script.onload = () => {
+        if (window.IN) {
+          window.IN.init();
+          window.IN.parse();
+        }
+      };
+      document.head.appendChild(script);
+    };
+  
+    loadLinkedInSDK();
+  }, []);
+
   return (
-    <div className="flex items-center justify-center min-h-screen bg-[#001427] overflow-x-hidden">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#001427] overflow-x-hidden">
       <div ref={ref} className="flex items-center bg-[#81d5ff] rounded-lg p-6">
         <div className="relative w-1/3 flex justify-center">
           <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-br from-purple-500 to-pink-500 rounded-full mix-blend-multiply"></div>
@@ -36,11 +108,11 @@ const About: React.FC<AboutProps> = ({ imageSrc, title, description }) => {
             />
           </div>
         </div>
-        <div className="w-1/2 ml-6"> {/* Adjusted width to 1/2 */}
-          <h2 className="text-4xl font-bold text-gray-800 mb-6 leading-tight whitespace-normal"> {/* Added whitespace-normal class */}
+        <div className="w-1/2 ml-6">
+          <h2 className="text-4xl font-bold text-gray-800 mb-6 leading-tight whitespace-normal">
             {title}
           </h2>
-          <p className="text-gray-600 leading-relaxed whitespace-normal"> {/* Added whitespace-normal class */}
+          <p className="text-gray-600 leading-relaxed whitespace-normal">
             {description}
           </p>
           <div className="mt-8">
@@ -55,7 +127,44 @@ const About: React.FC<AboutProps> = ({ imageSrc, title, description }) => {
           </div>
         </div>
       </div>
-    </div>
+      {/* Additional section */}
+      <div className="flex justify-center mt-8">
+        {/* Display the recently played tracks */}
+        <div className="w-full max-w-screen-xl mx-auto p-4 bg-white rounded-lg shadow-lg overflow-x-auto">
+          <h3 className="text-lg font-bold mb-4">Recently Played Tracks</h3>
+          <div className="tracks-container">
+            {recentlyPlayed.map((track, index) => (
+              <div key={index} className="track">
+                <div className="track-info">
+                  <div className="track-name">
+                    <strong className="whitespace-nowrap overflow-hidden overflow-ellipsis">{track.name}</strong>
+                  </div>
+                  <div>
+                    <img src={track.image} alt="No Album Cover :(" className="track-image" />
+                  </div>
+                  <div className="mt-2">
+                    <span className="text-sm text-gray-600">{track.artist['#text']}</span>
+                  </div>
+                </div>
+              </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        {/* Social Badges */}
+        <div className="linkedin-badge-container">
+          <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="VERTICAL" data-vanity="dev-patel-323017221" data-version="v1"></div>
+        </div>
+        <div className="github-badge-container">
+          <a href="https://github.com/devv64" target="_blank" rel="noopener noreferrer">
+            {/* https://github.com/Ashutosh00710/github-readme-activity-graph look here to choose theme later */}
+            <img src="https://github-readme-activity-graph.vercel.app/graph?username=devv64&theme=github" alt="GitHub Activity Graph" />
+          </a>
+        </div>
+
+
+
+      </div>
   );
 };
 
